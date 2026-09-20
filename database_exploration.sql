f30a381c7d9a785320d2ab49e1c91121974c9053
Darrow75 <darrow75@live.com>
2026-09-20 16:27:10 -0400

    Delete database_exploration.sql

--- database_exploration.sql
+++
@@ -1,9 +0,0 @@
---Explore all objects in the database
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
-
-
-
--- Explore all columns in the database
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'dim_customers'
